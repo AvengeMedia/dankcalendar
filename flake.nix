5f4447e7ab82c82f7badd608420a885f6cdf2a8c
Thomas Bemme <thomas.bemme@gmail.com>
2026-07-01 14:37:57 +0200

    chore: bump dcal flake to 0.2.0 (#32)

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
         pkgs:
         (
           let
-            version = "0.1.0";
+            version = "0.2.0";
           in
           (pkgs.buildGoModule.override { go = goForPkgs pkgs; }) {
             inherit version;
